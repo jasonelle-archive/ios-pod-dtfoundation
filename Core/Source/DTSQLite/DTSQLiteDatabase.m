91373daf35aafeaa94e626b9dce28a6a7276af57
Camilo <camilo@ninjas.cl>
2019-10-27 11:11:47 -0300

    fixed merge conflict

--- Core/Source/DTSQLite/DTSQLiteDatabase.m
+++ Core/Source/DTSQLite/DTSQLiteDatabase.m
@@ -71,11 +71,7 @@
 	
 	[self performBlock:^{
 		// set cancelling back after queue was cleared out
-<<<<<<< HEAD
-        self->_isCancelling = NO;
-=======
 		self->_isCancelling = NO;
->>>>>>> develop
 	}];
 	
 	[_queue setSuspended:NO];
